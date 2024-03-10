8d8db5918d225223cae50397322e1393c99cbe82
anderson rafael <franciscoandersonrafael@gmail.com>
2024-03-10 11:08:02 -0300

    remove logo fokus e dei 1rem na imgens

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
 
 focoBt.addEventListener('click', () => {
     //modo alternativo sem chamar a função alterarContexto
     //html.setAttribute('data-contexto', 'foco')
     //banner.setAttribute('src', '/imagens/foco.png')
     alterarContexto('foco')
+    focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
@@ -17,6 +19,7 @@ curtoBt.addEventListener('click', () => {
     //html.setAttribute('data-contexto', 'descanso-curto')
     //banner.setAttribute('src', 'imagens/descanso-curto.png')
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
@@ -24,9 +27,13 @@ longoBt.addEventListener('click', () => {
     //html.setAttribute('data-contexto', 'descanso-longo')
     //banner.setAttribute('src', '/imagens/descanso-longo.png')
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 })
 
 function alterarContexto(contexto) {
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
